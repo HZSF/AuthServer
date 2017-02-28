bdd07891c0b592871cf82f596e72d9c700401f77
yktangint <tang.yingkai@intellif.com>
2017-02-28 17:26:17 +0800

    annotations on webmvcconfig

--- src/main/java/com/weiwei/security/config/WebMvcConfig.java
+++ src/main/java/com/weiwei/security/config/WebMvcConfig.java
@@ -1,12 +1,10 @@
 package com.weiwei.security.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-//@Configuration
-//@SessionAttributes("authorizationRequest")
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 	@Override
